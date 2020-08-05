5e2bf9c900f3d21eedf1f888e70c3ea4381a9d95
Michael Grant <mdgrant116@gmail.com>
2020-08-05 18:20:42 -0400

    Lockscreen Finished

--- PracticeiOSLockScreen/ContentView.swift
+++ PracticeiOSLockScreen/ContentView.swift
@@ -8,9 +8,73 @@
 
 import SwiftUI
 
+
+struct LockScreenButton: View {
+    @State private var pressed = false
+    @State private var activate = false
+    
+    
+    let image: String
+    var body: some View {
+        Image(systemName: image)
+            .font(.title)
+            .frame(width: 50, height: 50)
+            .background(activate ? Color.white : Color.black.opacity(pressed ? 0.8 : 0.4))
+            .foregroundColor(activate ? .black : .white)
+            
+            .clipShape(Circle())
+            .scaleEffect(pressed ? 1.5 : 1)
+            .animation(.spring(response: 0.5, dampingFraction: 0.7))
+            .onLongPressGesture(minimumDuration: 0.4, pressing: { (bool) in
+                self.pressed = bool
+            }) {
+                self.activate.toggle()
+                self.pressed = false
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            GeometryReader { geo in
+                Image("bigsur")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(maxWidth: geo.size.width)
+                
+                Color.black.opacity(0.15)
+                VStack(spacing: 0) {
+                    Image(systemName: "lock.fill")
+                        .font(.largeTitle)
+                        .padding(.top , 60)
+                    Text("9:41")
+                        .font(.system(size: 92, weight: .thin))
+                    
+                    
+                    
+                    Text("August Not Friday, 2020")
+                        .font(.title)
+                        .offset(y: -10)
+                    Spacer()
+                    
+                    HStack {
+                        LockScreenButton(image: "flashlight.off.fill")
+                        Spacer()
+                        LockScreenButton(image: "camera.fill")
+                    }
+                    .padding([.leading, .trailing, .bottom])
+                    Capsule()
+                        .fill(Color.white)
+                        .frame(width: 150, height: 5)
+                        .padding(.bottom, 10)
+                }
+                    
+                .padding([.leading, .trailing])
+                .foregroundColor(.white)
+                
+            }.edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
